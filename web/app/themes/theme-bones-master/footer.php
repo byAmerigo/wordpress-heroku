50a07b554af93565231c7ec362668e25b48c28c6
Paola Collu <af46377@enelint.global>
2021-11-01 18:01:59 +0100

    resized footer logos

--- web/app/themes/theme-bones-master/footer.php
+++ web/app/themes/theme-bones-master/footer.php
@@ -1,8 +1,18 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 				<div class="d-flex justify-content-between">
 					<div>
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/timLogo.svg" alt="Tim Logo" />
-						<img src="<?php echo get_template_directory_uri(); ?>/library/images/poweredByLogo.svg" alt="Powered by Olivetti Logo" />
+						<img 
+							src="<?php echo get_template_directory_uri(); 
+							?>/library/images/timLogo.svg" 
+							alt="Tim Logo" 
+							style="width:220px; height:57px"
+						/>
+						<img 
+							src="<?php echo get_template_directory_uri(); 
+							?>/library/images/poweredByLogo.svg" 
+							alt="Powered by Olivetti Logo" 
+							style="width:180px; height:97px" 
+							/>
 					</div>
 					<div>
 						<p class="oli-footer-copyright-colour">© Copyright 2021 TIM S.p.A.</p>
