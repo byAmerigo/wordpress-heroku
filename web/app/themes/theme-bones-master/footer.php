4d7ec3b31c78b1266bb7bfdf64b34cb1da967915
Paola Collu <af46377@enelint.global>
2021-11-01 17:55:31 +0100

    added flexbox to footer

--- web/app/themes/theme-bones-master/footer.php
+++ web/app/themes/theme-bones-master/footer.php
@@ -1,7 +1,12 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-				<img src="<?php echo get_template_directory_uri(); ?>/library/images/timLogo.svg" alt="Tim Logo" />
-				<img src="<?php echo get_template_directory_uri(); ?>/library/images/poweredByLogo.svg" alt="Powered by Olivetti Logo" />
-
+				<div class="d-flex justify-content-between">
+					<div>
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/timLogo.svg" alt="Tim Logo" />
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/poweredByLogo.svg" alt="Powered by Olivetti Logo" />
+					</div>
+					<div>
+						<p class="oli-footer-copyright-colour">© Copyright 2021 TIM S.p.A.</p>
+					</div>
 			</footer>
 
 		</div>
