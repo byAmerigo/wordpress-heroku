43789185b183a84ef24bbe7a0f3806d94d84cc92
byAmerigo <byamerigo@gmail.com>
2021-11-01 11:21:28 +0100

    add theme

--- web/app/themes/colorway/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php       
-if (is_active_sidebar('primary-widget-area')) :
-    dynamic_sidebar('primary-widget-area'); 
-endif;
-// A second sidebar for widgets, just because.
-if (is_active_sidebar('secondary-widget-area')) :
-    dynamic_sidebar('secondary-widget-area'); 
-endif;
-   
