a8ce6beb1ba832d603c02421f3352e87c64edc94
byAmerigo <byamerigo@gmail.com>
2021-11-01 18:24:23 +0100

    add header

--- web/app/themes/theme-bones-master/header.php
+++ web/app/themes/theme-bones-master/header.php
@@ -51,7 +51,7 @@
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 				<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement" class="d-flex justify-content-between align-items-center oli-header">
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p style="font-size:30px" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p style="font-size:30px; color: #fff" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
 					<div>
