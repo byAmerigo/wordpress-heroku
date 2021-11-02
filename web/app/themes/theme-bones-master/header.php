2f30978b0c1b4d70aa900ce1f51fd8a4d72ec573
Paola Collu <af46377@enelint.global>
2021-11-02 13:48:29 +0100

    a

--- web/app/themes/theme-bones-master/header.php
+++ web/app/themes/theme-bones-master/header.php
@@ -52,12 +52,11 @@
 					 	<?php bloginfo('name'); ?>
 					</p>
 					<?php if(true) : ?>
-            <div class="dropdown show">
-              <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div style="width: 100px; height:30px; background-color:red;" />
-                <img src="<?php echo get_template_directory_uri(); ?>/library/images/gridMenu.svg" alt="Grid Menu" />
-              </a>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown button
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <a class="dropdown-item" href="#">Action</a>
                 <a class="dropdown-item" href="#">Another action</a>
                 <a class="dropdown-item" href="#">Something else here</a>
